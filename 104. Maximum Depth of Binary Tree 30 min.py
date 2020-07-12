217e05b2b43d7e643d1e9c24681204888aafda3b
wyu227 <66560290+wyu227@users.noreply.github.com>
2020-07-11 22:02:27 -0500

    Add files via upload

---
+++ 104. Maximum Depth of Binary Tree 30 min.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxDepth(self, root):
+        depth = 0
+        level = [root] if root else []
+        while level:
+            depth += 1
+            queue = []
+            for el in level:
+                if el.left:
+                    queue.append(el.left)
+                if el.right:
+                    queue.append(el.right)
+            level = queue
+            
+        return depth
